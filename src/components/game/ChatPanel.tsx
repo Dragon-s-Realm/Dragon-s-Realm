13c0c4a72e1f4ddf495f0d26df903ca4a48def03
santiago <98044979+devsantiag@users.noreply.github.com>
2025-12-04 22:52:04 -0300

    Fix UI distortion in user chat
    The chat interface was previously misaligned due to layout inconsistencies. This change addresses the distortion and ensures consistent user experience.

--- src/components/game/ChatPanel.tsx
+++ src/components/game/ChatPanel.tsx
@@ -43,9 +43,9 @@ export const ChatPanel = ({ messages, onSendMessage, currentPlayerId }: ChatPane
   return (
     // <div className="pixel-panel flex flex-col h-0 flex-1">
 
-<div className="pixel-panel flex flex-col flex-1 min-h-0">
+<div className="pixel-panel flex flex-col flex-1 min-h-0 h-[450px]"> 
 
-    {/*// <div className="pixel-panel flex flex-col h-0 flex-1">*/}
+     {/*<div className="pixel-panel flex flex-col h-0 flex-1">*/}
 
       {/* Cabeçalho do chat */}
       <div className="px-2 py-1 border-b-2 border-border">
